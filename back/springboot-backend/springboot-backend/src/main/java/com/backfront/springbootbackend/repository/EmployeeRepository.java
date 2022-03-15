cd5afd8afce429c0a9b491698d9f5b45cd741926
Rachid ZAHAR <rachid.zahar@sii.fr>
2022-03-15 16:22:20 +0100

    back ok et possibilité d'ajouter des users

---
+++ back/springboot-backend/springboot-backend/src/main/java/com/backfront/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.backfront.springbootbackend.repository;
+
+import com.backfront.springbootbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
