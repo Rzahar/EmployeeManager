e228543abafb34c8224d0367356a5a9e0467e7c4
Rachid ZAHAR <rachid.zahar@sii.fr>
2022-03-15 17:26:35 +0100

    find by id ok

--- back/springboot-backend/springboot-backend/src/main/java/com/backfront/springbootbackend/controller/EmployeeController.java
+++ back/springboot-backend/springboot-backend/src/main/java/com/backfront/springbootbackend/controller/EmployeeController.java
@@ -1,22 +1,25 @@
 package com.backfront.springbootbackend.controller;
 
+import com.backfront.springbootbackend.exception.ResourceNotFoundException;
 import com.backfront.springbootbackend.model.Employee;
 import com.backfront.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
     // Get all employees
-    @GetMapping("/employees")
+    @GetMapping
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
@@ -26,4 +29,14 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    // build get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable  long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(employee);
+    }
+
+
 }
